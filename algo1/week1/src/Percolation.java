59c13e523c3b80363d28cbabd2b8d78e0f3bfe21
ShivKJ <shivkj001@gmail.com>
2019-07-06 10:18:15 +0530

    changing tab to space in indentation policy

--- algo1/week1/src/Percolation.java
+++ algo1/week1/src/Percolation.java
@@ -3,120 +3,120 @@ import static java.util.Arrays.fill;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private final int                  n, upSentinal;
-	private final WeightedQuickUnionUF uf;
-	private final boolean[]            blocked;
-	private int                        openSites;
+    private final int                  n, upSentinal;
+    private final WeightedQuickUnionUF uf;
+    private final boolean[]            blocked;
+    private int                        openSites;
 
-	public Percolation(int n) {
-		this.n = numberGreaterThanZero(n);
-		this.upSentinal = n * n;
-		this.uf = new WeightedQuickUnionUF(n * n + 2);
-		this.blocked = blocked(n * n + 2);
-	}
+    public Percolation(int n) {
+        this.n = numberGreaterThanZero(n);
+        this.upSentinal = n * n;
+        this.uf = new WeightedQuickUnionUF(n * n + 2);
+        this.blocked = blocked(n * n + 2);
+    }
 
-	public void open(int i, int j) {
-		isValidIndex(i, j);
+    public void open(int i, int j) {
+        isValidIndex(i, j);
 
-		if (isOpen(i, j))
-			return;
+        if (isOpen(i, j))
+            return;
 
-		blocked[index(i, j)] = false;
-		openSites++;
+        blocked[index(i, j)] = false;
+        openSites++;
 
-		tryConnectUp(i, j);
-		tryConnectLeft(i, j);
-		tryConnectRight(i, j);
-		tryConnectDown(i, j);
+        tryConnectUp(i, j);
+        tryConnectLeft(i, j);
+        tryConnectRight(i, j);
+        tryConnectDown(i, j);
 
-	}
+    }
 
-	public boolean isOpen(int i, int j) {
-		isValidIndex(i, j);
+    public boolean isOpen(int i, int j) {
+        isValidIndex(i, j);
 
-		return !blocked[index(i, j)];
-	}
+        return !blocked[index(i, j)];
+    }
 
-	public boolean isFull(int i, int j) {
-		isValidIndex(i, j);
+    public boolean isFull(int i, int j) {
+        isValidIndex(i, j);
 
-		return uf.connected(upSentinal, index(i, j));
-	}
+        return uf.connected(upSentinal, index(i, j));
+    }
 
-	public int numberOfOpenSites() {
-		return openSites;
-	}
+    public int numberOfOpenSites() {
+        return openSites;
+    }
 
-	public boolean percolates() {
-		return uf.connected(upSentinal, upSentinal + 1);
-	}
+    public boolean percolates() {
+        return uf.connected(upSentinal, upSentinal + 1);
+    }
 
-	private int index(int i, int j) {
-		return n * (i - 1) + j - 1;
-	}
+    private int index(int i, int j) {
+        return n * (i - 1) + j - 1;
+    }
 
-	private void connect(int a1, int b1, int a2, int b2) {
-		uf.union(index(a1, b1), index(a2, b2));
-	}
+    private void connect(int a1, int b1, int a2, int b2) {
+        uf.union(index(a1, b1), index(a2, b2));
+    }
 
-	private void connectToDownSentinal(int c) {
-		connect(n + 1, 2, n, c);
-	}
+    private void connectToDownSentinal(int c) {
+        connect(n + 1, 2, n, c);
+    }
 
-	private void connectToUpSentinal(int c) {
-		connect(n + 1, 1, 1, c);
-	}
+    private void connectToUpSentinal(int c) {
+        connect(n + 1, 1, 1, c);
+    }
 
-	private void tryConnectUp(int i, int j) {
-		if (i == 1)
-			connectToUpSentinal(j);
-		else if (isOpen(i - 1, j))
-			connect(i, j, i - 1, j);
+    private void tryConnectUp(int i, int j) {
+        if (i == 1)
+            connectToUpSentinal(j);
+        else if (isOpen(i - 1, j))
+            connect(i, j, i - 1, j);
 
-	}
+    }
 
-	private void tryConnectLeft(int i, int j) {
-		if (j > 1 && isOpen(i, j - 1))
-			connect(i, j, i, j - 1);
-	}
+    private void tryConnectLeft(int i, int j) {
+        if (j > 1 && isOpen(i, j - 1))
+            connect(i, j, i, j - 1);
+    }
 
-	private void tryConnectRight(int i, int j) {
-		if (j < n && isOpen(i, j + 1))
-			connect(i, j, i, j + 1);
-	}
+    private void tryConnectRight(int i, int j) {
+        if (j < n && isOpen(i, j + 1))
+            connect(i, j, i, j + 1);
+    }
 
-	private void tryConnectDown(int i, int j) {
+    private void tryConnectDown(int i, int j) {
 
-		if (i == n)
-			connectToDownSentinal(j);
-		else if (isOpen(i + 1, j))
-			connect(i, j, i + 1, j);
+        if (i == n)
+            connectToDownSentinal(j);
+        else if (isOpen(i + 1, j))
+            connect(i, j, i + 1, j);
 
-	}
+    }
 
-	private static boolean[] blocked(int n) {
-		boolean[] out = new boolean[n];
+    private static boolean[] blocked(int n) {
+        boolean[] out = new boolean[n];
 
-		fill(out, true);
+        fill(out, true);
 
-		return out;
-	}
+        return out;
+    }
 
-	public static int numberGreaterThanZero(int n) {
-		if (n < 1)
-			throw new IllegalArgumentException();
+    public static int numberGreaterThanZero(int n) {
+        if (n < 1)
+            throw new IllegalArgumentException();
 
-		return n;
-	}
+        return n;
+    }
 
-	private void isValidIndex(int i, int j) {
-		isValidIndex(i);
-		isValidIndex(j);
-	}
+    private void isValidIndex(int i, int j) {
+        isValidIndex(i);
+        isValidIndex(j);
+    }
 
-	private void isValidIndex(int i) {
-		if (i < 1 || i > n)
-			throw new IllegalArgumentException("row/column=" + i);
-	}
+    private void isValidIndex(int i) {
+        if (i < 1 || i > n)
+            throw new IllegalArgumentException("row/column=" + i);
+    }
 
 }
--- algo1/week1/src/PercolationStats.java
+++ algo1/week1/src/PercolationStats.java
@@ -9,61 +9,63 @@ import java.util.PrimitiveIterator.OfInt;
 
 import edu.princeton.cs.algs4.StdStats;
 
-public class PercolationStats {
-	private final int      trials;
-	private final double[] outcome;
+public final class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
 
-	public PercolationStats(int n, int trials) {
-		Percolation.numberGreaterThanZero(n);
-		this.trials = Percolation.numberGreaterThanZero(trials);
-		this.outcome = process(n, trials);
-	}
+    private final int      trials;
+    private final double[] outcome;
 
-	public double mean() {
-		return StdStats.mean(outcome);
-	}
+    public PercolationStats(int n, int trials) {
+        Percolation.numberGreaterThanZero(n);
+        this.trials = Percolation.numberGreaterThanZero(trials);
+        this.outcome = process(n, trials);
+    }
 
-	public double stdDev() {
-		return StdStats.stddev(outcome);
-	}
+    public double mean() {
+        return StdStats.mean(outcome);
+    }
 
-	public double confidenceLo() {
-		return mean() - 1.96 * StdStats.stddev(outcome) / sqrt(trials);
-	}
+    public double stddev() {
+        return StdStats.stddev(outcome);
+    }
 
-	public double confidenceHi() {
-		return mean() + 1.96 * StdStats.stddev(outcome) / sqrt(trials);
-	}
+    public double confidenceLo() {
+        return mean() - CONFIDENCE_95 * StdStats.stddev(outcome) / sqrt(trials);
+    }
 
-	private static double[] process(int n, int trials) {
-		return generate(() -> process(n)).limit(trials).toArray();
-	}
+    public double confidenceHi() {
+        return mean() + CONFIDENCE_95 * StdStats.stddev(outcome) / sqrt(trials);
+    }
 
-	private static double process(int n) {
-		int blocks = n * n;
-		Percolation p = new Percolation(n);
+    private static double[] process(int n, int trials) {
+        return generate(() -> process(n)).limit(trials).toArray();
+    }
 
-		OfInt itr = stream(permutation(blocks)).iterator();
+    private static double process(int n) {
+        int blocks = n * n;
+        Percolation p = new Percolation(n);
 
-		while (!p.percolates()) {
-			int s = itr.nextInt();
-			int i = 1 + s / n;
-			int j = 1 + s % n;
+        OfInt itr = stream(permutation(blocks)).iterator();
 
-			p.open(i, j);
-		}
+        while (!p.percolates()) {
+            int s = itr.nextInt();
+            int i = 1 + s / n;
+            int j = 1 + s % n;
 
-		return (1. * p.numberOfOpenSites()) / blocks;
+            p.open(i, j);
+        }
 
-	}
+        return (1. * p.numberOfOpenSites()) / blocks;
 
-	public static void main(String[] args) {
-		int n = parseInt(args[0]), trials = parseInt(args[1]);
+    }
 
-		PercolationStats stats = new PercolationStats(n, trials);
-		printf("%-23s = %f\n", "mean", stats.mean());
-		printf("%-23s = %f\n", "stddev", stats.stdDev());
-		printf("%-23s = [%f, %f]\n", "95% confidence interval", stats.confidenceLo(), stats.confidenceHi());
+    public static void main(String[] args) {
+        int n = parseInt(args[0]), trials = parseInt(args[1]);
 
-	}
+        PercolationStats stats = new PercolationStats(n, trials);
+        printf("%-23s = %f\n", "mean", stats.mean());
+        printf("%-23s = %f\n", "stddev", stats.stddev());
+        printf("%-23s = [%f, %f]\n", "95% confidence interval", stats.confidenceLo(), stats.confidenceHi());
+
+    }
 }
