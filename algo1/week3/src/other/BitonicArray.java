e2b48fe64fc896a75230c9e0e8a59312e36a10a7
ShivKJ <shivkj001@gmail.com>
2019-07-10 10:32:25 +0530

    making algo to take 2Log(n) time

--- algo1/week3/src/other/BitonicArray.java
+++ algo1/week3/src/other/BitonicArray.java
@@ -1,5 +1,6 @@
 package other;
 
+import static java.lang.Math.max;
 import static java.util.Arrays.binarySearch;
 
 public final class BitonicArray {
@@ -17,26 +18,21 @@ public final class BitonicArray {
         int mid = (from + to) >> 1;
         int e = arr[mid];
 
-        if (k < e)
-            if (arr[mid - 1] < arr[mid]) {
-
-                int index = binarySearch(arr, from, mid, k);// mid is exclusive here
-
-                return index > -1 ? index : search(arr, k, mid + 1, to);
-
-            } else {
-                int index = reverseBinarySearch(arr, k, mid + 1, to);
-
-                return index > -1 ? index : search(arr, k, from, mid - 1);
-            }
-        else if (k > e)
+        if (k < e) {
+            int index = bs(arr, k, from, mid - 1);
+            return index > -1 ? index : rbs(arr, k, mid + 1, to);
+        } else if (k > e)
             return arr[mid - 1] < arr[mid] ? search(arr, k, mid + 1, to) : search(arr, k, from, mid - 1);
         else
             return mid;
 
     }
 
-    public static int reverseBinarySearch(int[] arr, int k, int lo, int high) {
+    public static int bs(int[] arr, int k, int lo, int high) {
+        return max(binarySearch(arr, lo, high + 1, k), -1);
+    }
+
+    public static int rbs(int[] arr, int k, int lo, int high) {
 
         while (lo <= high) {
             int mid = (lo + high) >> 1;
