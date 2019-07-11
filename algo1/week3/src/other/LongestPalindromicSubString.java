57b4f84131b751fafde287f30ba7aadcc9c304cf
ShivKJ <shivkj001@gmail.com>
2019-07-11 20:22:27 +0530

    adding documentation for LongestPalindromicSubString

--- algo1/week3/src/other/LongestPalindromicSubString.java
+++ algo1/week3/src/other/LongestPalindromicSubString.java
@@ -1,37 +1,67 @@
 package other;
 
-import static java.lang.String.copyValueOf;
+import static java.lang.Integer.compare;
 import static java.util.Arrays.copyOfRange;
 
+import java.util.stream.IntStream;
+
 public final class LongestPalindromicSubString {
+    /**
+     * To find largest palendromic in a String
+     * in string "abcdcdz", largest palendromic string is "cdc"
+     * and in "abcddc" it is "cddc".
+     * 
+     * @param string
+     * @return
+     */
     public static String longestPalindrome(String string) {
         return string.isEmpty() ? string : search(string.toCharArray());
     }
 
-    private static String search(char[] str) {
-        int[] arr = { 0, 0 };
+    private static String search(char[] arr) {
+        return IntStream.range(0, arr.length)
+                        .mapToObj(i -> largestPelindromeAround(arr, i))
+                        .max(Range::compareTo)
+                        .map(r -> copyOfRange(arr, r.startIdx, r.endIdx + 1))
+                        .map(String::copyValueOf)
+                        .get();
 
-        for (int i = 0; i < str.length; i++) {
-            int[] a = largestPelindromeAround(str, i);
+    }
 
-            if (isGreater(a, arr))
-                arr = a;
+    private static class Range implements Comparable<Range> {
+        private final int startIdx, endIdx;
+
+        Range(int startIdx, int endIdx) {
+            this.startIdx = startIdx;
+            this.endIdx = endIdx;
         }
 
-        return copyValueOf(copyOfRange(str, arr[0], arr[1] + 1));
+        @Override
+        public int compareTo(Range o) {
+            return compare(endIdx - startIdx, o.endIdx - o.startIdx);
+        }
+
     }
 
-    private static boolean isGreater(int[] a, int[] b) {
-        return a[1] - a[0] > b[1] - b[0];
-    }
-
-    private static int[] largestPelindromeAround(char[] str, int idx) {
-        int[] a = pelindromeExpansion(str, idx, idx);
+    /**
+     * Finding largest palindromic string having its middle
+     * on index idx.
+     * 
+     * Usually palindromic string is of odd length
+     * but in the case of even length, two middle element
+     * will be same.
+     * 
+     * @param str
+     * @param idx
+     * @return
+     */
+    private static Range largestPelindromeAround(char[] str, int idx) {
+        Range a = pelindromeExpansion(str, idx, idx);
 
         if (idx < str.length - 1 && str[idx] == str[idx + 1]) {
-            int[] b = pelindromeExpansion(str, idx, idx + 1);
+            Range b = pelindromeExpansion(str, idx, idx + 1);
 
-            if (isGreater(b, a))
+            if (b.compareTo(a) > 0)
                 a = b;
 
         }
@@ -39,7 +69,16 @@ public final class LongestPalindromicSubString {
         return a;
     }
 
-    private static int[] pelindromeExpansion(char[] arr, int left, int right) {
+    /**
+     * Finding left and right index of palindromic string having 
+     * middle index "left" and "right". 
+     * 
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private static Range pelindromeExpansion(char[] arr, int left, int right) {
         int n = arr.length;
 
         while (left > 0 && right < n - 1 && arr[left - 1] == arr[right + 1]) {
@@ -47,10 +86,13 @@ public final class LongestPalindromicSubString {
             right++;
         }
 
-        return new int[] { left, right };
+        return new Range(left, right);
     }
 
     public static void main(String[] args) {
         System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("aa"));
+        System.out.println(longestPalindrome("a"));
+        System.out.println(longestPalindrome("abcddcsz"));
     }
 }
